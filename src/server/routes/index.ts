76a90527ed8aba00ab5cdd593d7202a5688da875
LeonardoMonsuete <leonardo.monsuete@outlook.com>
2023-04-28 22:38:33 -0300

    feat: Begining controllers creation, starting with city Controller

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
+
+import { CitiesController } from './../controllers';
 const router = Router();
 
 router.get('/', (req, res) => {
   
   return res.send('Olá Mundo !');
 });
+
+router.post('/cidades', CitiesController.create);
 
 export { router };
